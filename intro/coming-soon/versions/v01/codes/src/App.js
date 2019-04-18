c8784a49f37434f4d9a4523ea8d36b208231757e
ibare <kim.mintae@woowahan.com>
2019-04-18 14:26:43 +0900

    coming-soon 추가

---
+++ intro/coming-soon/versions/v01/codes/src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Coming Soon</h1>
+        <p>
+          곧 찾아 뵙겠습니다. 🐴
+        </p>
+      </div>
+    )
+  }
+}
